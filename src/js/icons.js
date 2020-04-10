83a4f9686bb3e60104e30038726ae69909129b0f
aaaragon <aragonandre333@gmail.com>
2020-04-09 22:49:01 -0400

    rr

---
+++ src/js/icons.js
@@ -0,0 +1,9 @@
+import fontawesome from "@fontawesome/fontawesome";
+
+import faClock from "@fontawesome/fontawesome-free-regular/faClock";
+
+fontawesome.config = {
+	autoReplaceSvg: "nest"
+};
+
+fontawesome.library.add(faClock);
